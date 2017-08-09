077a1b497421d5846dbe6dd7daf64d3c96363201
mikesiers <msiers@csu.edu.au>
2017-08-09 13:40:16 +1000

    test: Implemented Branch class unit tests.
    These test the constructor and the string representation.
    
    See GitHub Issue #3.

--- tests/branch-tests.py
+++ tests/branch-tests.py
@@ -6,10 +6,19 @@ import unittest
 class test_branch_class(unittest.TestCase):
   def test_parent(self):
     # Check that the parent is correctly assigned.
-    pass
+    parent = Node(attribute_name='parent_attr')
+    branch = Branch(parent=parent)
+    self.assertEqual(branch.parent, parent)
+
   def test_child(self):
     # Check that the child is correctly assigned.
-    pass
+    child = Node(attribute_name='child_attr')
+    branch = Branch(child=child)
+    self.assertEqual(branch.child, child)
+
   def test_string(self):
     # Check that the string representation is as expected.
-    pass
+    test = Split_Test(attribute_type='numerical', attribute='age',
+      split_value=50, operator='>')
+    branch = Branch(split_test=test)
+    self.assertEqual(str(branch), 'age > 50')
